ded9e009e05264341379e2aff8ea9cb1ec667ec6
jongkyuh <jongkyuh@naver.com>
2024-07-30 22:01:33 +0900

    2. JPA 실행 테스트 및 영속성 컨텍스트 동작 확인
    영속성 컨텍스트를 사용하게 될때의 주요 이점
    1차 캐시, 영속 엔티티의 동일성 보장, 쓰기지연, 변경감지 등을 예제를 통해 학습했다. main 에서 확인하며 지워서 코드가 남아있지 않지만, 노션 페이지의 영속성 컨텍스트 1,2페이지를 통해서 코드를 확인 할 수 있다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,42 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
-
+        // 1. 엔티티매니저팩토리 생성
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        // 2. 엔티티 매니저 생성
+        EntityManager em = emf.createEntityManager();
+
+        // 3. 트랜잭션 생성
+        EntityTransaction tx = em.getTransaction();
+        // 4. 트랜잭션 시작
+        tx.begin();
+
+        //code.....
+        try{
+            //영속
+           Member member = new Member(200L,"member200");
+           em.persist(member);
+
+           em.flush();  // 이 시점에 즉시 DB에 쿼리문이 나가서 반영된다.
+
+            System.out.println("================================");
+            // 커밋시점에 반영
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+
+        }
+        // 6. 전체 어플리케이션이 종료하면 엔티티매니저팩토리 닥기
         emf.close();
+
+
     }
 }
