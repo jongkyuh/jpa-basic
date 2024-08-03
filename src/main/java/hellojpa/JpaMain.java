f7818d35e9092e81306ca28aa1c136ce6dc704cb
jongkyuh <jongkyuh@naver.com>
2024-08-03 22:59:05 +0900

    4. 키 매핑
    IDENTITY방식, TABLE방식, SEQUENCE방식, AUTO방식이 있다.
    IDENTITY방식은 DB 자체에서 키값을 설정하도록 일임하는것이기 때문에 INSERT하기전에는 키값을 알 수 없다. 그래서 JPA는 예외적으로 IDENTITY방식의 엔티티를 INSERT할 때 미리 INSERT문을 날린다.
    TABLE,SEQUENCE방식은 미리 allocationSize의 크기만큼 키값을 가져와 성능 최적화를 할 수 있다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,12 +20,24 @@ public class JpaMain {
 
         //code.....
         try{
-            Member member = new Member();
-            member.setId(3L);
-            member.setUsername("A");
-            member.setRoleType(RoleType.GUSET);
+            Member member1 = new Member();
+            Member member2 = new Member();
+            Member member3 = new Member();
+         //   member.setId("ID_A");
+            member1.setUsername("A");
+            member2.setUsername("A");
+            member3.setUsername("A");
 
-            em.persist(member);
+            System.out.println("============================================");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+
+            System.out.println("============================================");
+            System.out.println("member = " + member1.getId());
+            System.out.println("커밋");
             // 커밋시점에 반영
             tx.commit();
         }catch (Exception e){
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -7,30 +7,20 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    @Column(name = "name",nullable = false,length = 10,columnDefinition = "varchar(100) default 'empty'")      //객체는 username, db는 name. not null 제약이 걸린다.
+
+
+    @Column   //객체는 username, db는 name. not null 제약이 걸린다.
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)    //enum 타입,기본값인 ORDINARY는 쓰지말자
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)   // 날짜 타입,생성일자
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)   // 수정날짜
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate;    // 최신버전에서는 @Temporal을 쓰지 않아도 된다.
-    private LocalDateTime testLocalDateTime;
-
-    @Lob    // varchar를 넘어서는 큰 데이터르 넘기고 싶을 떄
-    private String description;
-
-
-
-    public Member() {
-    }
 
     public Long getId() {
         return id;
@@ -48,43 +38,8 @@ public class Member {
         this.username = username;
     }
 
-    public Integer getAge() {
-        return age;
+    public Member() {
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
