b4c1a143be132014f32d5222acc8e129b9ad9e56
jongkyuh <jongkyuh@naver.com>
2024-08-11 20:43:13 +0900

    6. 다대일 매핑
    다대일 매핑을 @JoinTable 어노테이션을 활용하면 된다. 그러나 이렇게 하면 딱 FK값들만 설정되기에 컬럼을 추가할 수 없다. 그래서 아예 테이블을 하나 만들어 다대일,일대다 관계로 푸는게 맞다

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -26,6 +28,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
