9d1a4e0d09b0c5f8db3608cde336f3f20d76b708
jongkyuh <jongkyuh@naver.com>
2024-08-14 21:43:15 +0900

    11. 영속성 전이와 고아 객체
    cascade를 통해서 같이 제거되거나 추가될때 함계 persist,removce 될수 있으며 orphanremoval = ture하게 되면 컬렉션에서 제거 될 때 자식 엔티티가 삭제 된다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -24,34 +24,22 @@ public class JpaMain {
 
         //code.....
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
-
+            em.persist(parent);     // child도 persist된다.
 
             em.flush();
             em.clear();
 
-            //Member m = em.find(Member.class, member1.getId());
-            List<Member> selectMFromMemberM = em.createQuery("select m from Member m join fetch m.team", Member.class)
-                    .getResultList();
-            // SQL: SELECT * FROM MEMBER
-            // SQL: SELECT * FROM TEAM WHERE TEAM_ID = .....
+            Parent findParent = em.find(Parent.class, parent.getId());
+          //  findParent.getChildList().remove(0);    // 0번째 인덱스 child 삭제, 실제 db에서 remove된다.
+
+            em.remove(findParent);  // child도 같이 remove
 
 
             // 커밋시점에 반영
